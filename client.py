58260448cd200fd9dd72db169b80787c5c1a272f
Kyrstn Hall <kyrstn.hall@gmail.com>
2023-11-23 14:01:14 -0800

    step 6, 7, 11

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ import socket
 from datetime import datetime
 import sys
 
-
 # Time operations in python
 # timestamp = datetime.fromisoformat(isotimestring)
 
@@ -15,10 +14,36 @@ MAC = ":".join(["{:02x}".format((uuid.getnode() >> ele) & 0xFF) for ele in range
 SERVER_IP = "10.0.0.100"
 SERVER_PORT = 9000
 
-# Parse the client messages - gets MAC 
+# [0]OFFER [1]A1:30:9B:D3:CE:18 [2]192.168.45.1 [3]2022-02-02T11:42:08.761340
+
+# Parse the client messages - gets response  
 def parse_message(message):
+    response = message.split(' ', 1)[0]
+    return response
+
+# Parse the client messages - gets MAC  
+def get_MAC(message):
     response = message.split(' ', 1)[1]
     return response
+
+# Parse the client messages - gets IP  
+def get_IP(message):
+    response = message.split(' ', 1)[2]
+    return response
+
+# Parse the client messages - gets timestamp  
+def get_time(message):
+    response = message.split(' ', 1)[3]
+    return response
+
+def menu():
+    print("Choose from the following choices: ")
+    print("1. Release")
+    print("2. Renew")
+    print("3. Quit")
+
+    choice = int(input())
+    return choice
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -26,38 +51,91 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = "DISCOVER " + MAC
 clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
 
-# LISTENING FOR RESPONSE - message is response, rest is client address
+# LISTENING FOR RESPONSE 
 message, clientAddress = socket.recvfrom(4096) 
 
-# if message == offer
-    # MAC address in message == client's MAC address
-        # yes - check if time stamp is expired
-        # no - send request message containing client's MAC, IP, and timestamp offered by server
-# if message == decline
-    # display declined message, program terminates
-# if message == acknowledge
+# splitting the response into 3 parts
+parsed_message = parse_message(message) 
+response_MAC = get_MAC(message)
+response_IP = get_IP(message)
+response_time = get_time(message)
+timestamp = datetime.fromisoformat(response_time)
 
-#current_time = datetime.timestamp(datetime.now())
-#timestamp = datetime.fromisoformat(current_time)
-parsed_message = parse_message(message)
-
-if message == "OFFER":
-     if clientAddress == MAC:
+if parsed_message == "OFFER":
+     if response_MAC == MAC:
          pass
      else:
-         # Sending REQUEST message
-         message = "REQUEST " + MAC + " " + IP + " " + timeStamp
-elif message == "DECLINE":
+         message = "REQUEST " + MAC + " " + response_IP + " " + response_time
+         clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+elif parsed_message == "DECLINE":
     print("Request Denied!")
-    clientSocket.close()
     sys.exit()
+elif parsed_message == "ACKNOWLEDGE":
+     if response_MAC != MAC:
+         print("Request Denied!")
+         sys.exit()
+     else:
+         print("Your IP address is: " + response_IP + " which will expire at " + response_time)
+         client_choice = menu()
+         if client_choice == 1: # release
+            message = "RELEASE " + MAC + " " + response_IP + " " + response_time
+            clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+         elif client_choice == 2: # renew
+             message = "RENEW " + MAC + " " + response_IP + " " + response_time
+             clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+         else: # quit
+             clientSocket.close()
+             sys.exit()
+    
 
+            # combining logic from above 
+try:
+    while True:
+        # Sending DISCOVER message
+        print("Client: Discovering...")    
+        message = "DISCOVER " + MAC
+        clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+        
+        # LISTENING FOR RESPONSE 
+        message, clientAddress = socket.recvfrom(4096) 
 
+        # splitting the response into 3 parts
+        parsed_message = parse_message(message) 
+        response_MAC = get_MAC(message)
+        response_IP = get_IP(message)
+        response_time = get_time(message)
+        timestamp = datetime.fromisoformat(response_time)
+        
+        if parsed_message == "OFFER":
+            if response_MAC == MAC:
+                pass
+            else:
+                print("Client: Requesting " + response_IP + " IP address")
+                message = "REQUEST " + MAC + " " + response_IP + " " + response_time
+                clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+        elif parsed_message == "DECLINE":
+            print("Request Denied!")
+            sys.exit()
+        elif parsed_message == "ACKNOWLEDGE":
+            if response_MAC != MAC:
+                print("Request Denied!")
+                sys.exit()
+            else:
+                print("Your IP address is: " + response_IP + " which will expire at " + response_time)
+                client_choice = menu()
+                if client_choice == 1: # release
+                    message = "RELEASE " + MAC + " " + response_IP + " " + response_time
+                    clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+                elif client_choice == 2: # renew
+                    message = "RENEW " + MAC + " " + response_IP + " " + response_time
+                    clientSocket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+                else: # quit
+                    clientSocket.close()
+                    sys.exit()
 
-# Sending REQUEST message
-
-# Sending RELEASE message
-
-# Sending RENEW message
-
-# Can receive decline, offer, acknowledge and list all records from server
+except OSError:
+    pass
+except KeyboardInterrupt:
+    pass
+  
+clientSocket.close()
