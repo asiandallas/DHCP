1ab030b46f21eeaf576a3b118f78435e156bdc0a
Kyrstn Hall <kyrstn.hall@gmail.com>
2023-11-25 00:32:33 -0800

    fixed client and tried figuring out server

--- client.py
+++ client.py
@@ -16,24 +16,34 @@ SERVER_PORT = 9000
 
 # [0]OFFER [1]A1:30:9B:D3:CE:18 [2]192.168.45.1 [3]2022-02-02T11:42:08.761340
 
+# Parse the server messages   
+def parse_message(message):
+    message_parts = message.decode().split()
+    response = message_parts[0]
+    mac = message_parts[1]
+    ip = message_parts[2]
+    time = message_parts[3]
+
+    return message_parts
+
 # Parse the client messages - gets response  
 def parse_message(message):
-    response = message.split(' ', 1)[0]
+    response = message.split(' ')[0]
     return response
 
 # Parse the client messages - gets MAC  
 def get_MAC(message):
-    response = message.split(' ', 1)[1]
+    response = message.split(' ')[1]
     return response
 
 # Parse the client messages - gets IP  
 def get_IP(message):
-    response = message.split(' ', 1)[2]
+    response = message.split(' ')[2]
     return response
 
 # Parse the client messages - gets timestamp  
 def get_time(message):
-    response = message.split(' ', 1)[3]
+    response = message.split(' ')[3]
     return response
 
 def menu():
@@ -57,11 +67,11 @@ try:
         # LISTENING FOR RESPONSE 
         message, clientAddress = socket.recvfrom(4096) 
 
-        # splitting the response into 3 parts
+        # splitting the response - parsed_message is a list
         parsed_message = parse_message(message) 
-        response_MAC = get_MAC(message)
-        response_IP = get_IP(message)
-        response_time = get_time(message)
+        response_MAC = parsed_message[0]
+        response_IP = parsed_message[1]
+        response_time = parsed_message[2]
         timestamp = datetime.fromisoformat(response_time)
         
         if parsed_message == "OFFER":
